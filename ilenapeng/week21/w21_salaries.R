3e79dadd52ce14b23dc7f5aae1a95cce84304460
rfaridi <rushad.16@gmail.com>
2021-10-27 23:18:06 +0600

    New changes

--- ilenapeng/week21/w21_salaries.R
+++ ilenapeng/week21/w21_salaries.R
@@ -3,6 +3,7 @@ library(extrafont)
 library(ggtext)
 
 survey <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-05-18/survey.csv')
+saveRDS(survey, "./survey.rds")
 
 # its dataframe with 26232 obs and 18 vars
 
@@ -125,7 +126,7 @@ ggplot(data=df, aes(x=highest_edu,
      subtitle="Self-reported earnings for <b><span style='color:#346F5B'>male</span></b>, <b><span style='color:#364196'>female</span></b> and <b><span style='color:#B9A513'>non-binary</span></b> individuals",
     caption="Data from Ask The Manager blog \n Graphic by Ilena Peng for #TidyTuesday") + 
    theme_minimal() +
-   theme(text=element_text(family="Roboto"))
+   theme(text=element_text(family="Roboto"),, axis.title=element_blank())
 
 
 ggplot(data=df, aes(x=highest_edu, 
@@ -140,8 +141,7 @@ ggplot(data=df, aes(x=highest_edu,
        subtitle="Self-reported earnings for <b><span style='color:#346F5B'>male</span></b>, <b><span style='color:#364196'>female</span></b> and <b><span style='color:#B9A513'>non-binary</span></b> individuals",
        caption="Data from Ask The Manager blog \n Graphic by Ilena Peng for #TidyTuesday") +
   theme_minimal() +
-theme(text=element_text(family="Roboto"), axis.title=element_blank(), axis.text=element_text(color='Black'), panel.grid.major.x = element_line(color="white"),
-        plot.title=element_text(size=20), plot.subtitle=element_markdown(size=16), plot.caption=element_text(size=8), plot.margin=margin(0.5,0.5,0.5,0.5, unit = "cm"))
+theme(text=element_text(family="Roboto"), axis.title=element_blank(), axis.text=element_text(color='Black'),panel.grid.major.x = element_line(color="white"), plot.title=element_text(size=20), plot.subtitle=element_markdown(size=16), plot.caption=element_text(size=8), plot.margin=margin(0.5,0.5,0.5,0.5, unit = "cm"))
 
 print(plot)
 ggsave("w21_salaries.png",width=12, height=9, unit="in")
